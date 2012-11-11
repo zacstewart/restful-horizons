430963a9d4b3c742fc1b031e0dd162e231e06e91
Zac Stewart <zgstewart@gmail.com>
2012-11-11 01:03:46 -0500

    Create HTML interface

--- horizons.rb
+++ horizons.rb
@@ -1,6 +1,16 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
 class Horizons < Sinatra::Base
+  def client
+    @_client ||= Tengai::Client.new
+  end
+
+  get '/' do
+    File.read('public/index.html')
+  end
   get '/bodies/:body' do
-    client = Tengai::Client.new
     body = Tengai::Body.find(client, params['body'])
     body.data
   end
